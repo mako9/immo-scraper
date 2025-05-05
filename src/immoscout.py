eb0be1e0cfac3468ff36e31351656989fe67d11a
Mario Kohlhoff <mario.kohlhoff@edag.com>
2025-05-05 22:51:56 +0200

    Adapt parsing to new HTML structure

--- src/immoscout.py
+++ src/immoscout.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,11 +11,13 @@ from src.url import get_url_without_page, get_url_with_page
 from src.immo_data import ImmoData, ReportType
 from src.immo_platform import ImmoPlatform
 
-URL = 'https://www.immobilienscout24.de/Suche/radius/+++?centerofsearchaddress=Fulda%20(Kreis);;;1276007005017;;***&geocoordinates=50.54398;9.7184;§§§.0&enteredFrom=one_step_search&pagenumber=$$$&price=-###'
+BASE_URL = "https://www.immobilienscout24.de"
+URL = "https://www.immobilienscout24.de/Suche/radius/+++?centerofsearchaddress=Fulda%20(Kreis);;;1276007005017;;***&geocoordinates=50.54398;9.7184;§§§.0&enteredFrom=one_step_search&pagenumber=$$$&price=-###"
 
 executor_url: str = None
 session_id: str = None
 driver = None
+
 
 def get_immoscout_results():
     house_listings = _get_results_of_type(ReportType.HOUSE)
@@ -38,17 +41,36 @@ def _get_results_of_type(type: ReportType):
     while True:
         url = get_url_with_page(url_without_page, index)
         print(url)
-        soup = _get_soup(url)
-        new_listings = soup.find_all('li', {'class': 'result-list__listing'})
-        listings += new_listings
+        soup = _get_soup(url, By.ID, "result-list-content")
+        new_listings = soup.find_all("div", {"class": "listing-card"})
+        listings += list(
+            map(
+                lambda x: (
+                    x,
+                    _get_listing_soup(x),
+                ),
+                new_listings,
+            )
+        )
         if len(new_listings) < 20:
             break
         index += 1
 
-    return list(map(lambda x: _get_immo_data(type, x), listings))
+    return list(
+        filter(
+            lambda x: x is not None, map(lambda x: _get_immo_data(type, x), listings)
+        )
+    )
 
 
-def _get_soup(url):
+def _get_listing_soup(listing):
+    href = _get_href(listing)
+    if href is None:
+        return None
+    return _get_soup(f"{BASE_URL}{href}", By.CLASS_NAME, "main-criteria-container")
+
+
+def _get_soup(url, type, name):
     global driver  # Declare the variables as nonlocal
     # Send the request and get the HTML response
     if driver is None:
@@ -60,34 +82,40 @@ def _get_soup(url):
 
     # Once we need to accept manually the Captcha. The browser session will then be reused.
 
-    wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'result-list-content')))
+    wait.until(EC.presence_of_element_located((type, name)))
 
     html = driver.page_source
 
     # Parse the HTML response with BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
 
     return soup
 
 
 def _get_immo_data(type, listing):
-    infos = listing.findAll(
-        'dd', {'class': 'font-highlight font-tabular'})
+    infos = listing[0].findAll("dd", {"class": "font-bold"})
+    if not infos:
+        return None
     price = infos[0].text.strip()
     living_area = None
     if type == ReportType.HOUSE:
         living_area = infos[1].text.strip()
-        land_area = infos[3].text.strip()
+        land_area = infos[3].text.strip() if len(infos) > 3 else None
     else:
         land_area = infos[1].text.strip()
 
     return ImmoData(
-        link='https://www.immobilienscout24.de' +
-        listing.find('a')['href'],
-        title=listing.find('h2').text.strip(),
+        link=BASE_URL + _get_href(listing[0]),
+        title=listing[0].find("h2", {"data-testid": "headline"}).text.strip(),
         price=price,
         living_area=living_area,
         land_area=land_area,
         type=type,
-        distance=None
+        distance=None,
     )
+
+
+def _get_href(listing) -> Optional[str]:
+    element = listing.find("a")
+    if element:
+        return element["href"]
