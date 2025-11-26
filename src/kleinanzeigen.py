370b133fb1193e2fbb588c33c251f440803d7d83
Mario Kohlhoff <mario.kohlhoff@edag.com>
2025-11-26 07:52:22 +0100

    Fix infinite fetching of data in kleinanzeigen.py

--- src/kleinanzeigen.py
+++ src/kleinanzeigen.py
@@ -13,12 +13,13 @@ from src.url import get_url_without_page, get_url_with_page
 from src.immo_data import ImmoData, ReportType
 from src.immo_platform import ImmoPlatform
 
-BASE_URL = 'https://www.kleinanzeigen.de'
-URL = f'{BASE_URL}/+++/***/preis::###$$$/c20^^^l4619r§§§'
+BASE_URL = "https://www.kleinanzeigen.de"
+URL = f"{BASE_URL}/+++/***/preis::###$$$/anzeige:angebote/c20^^^l4619r§§§"
 
 executor_url: str = None
 session_id: str = None
 driver = None
+
 
 def get_kleinanzeigen_results():
     house_listings = _get_results_of_type(ReportType.HOUSE)
@@ -32,9 +33,9 @@ def get_kleinanzeigen_results():
 def _get_url_without_page(type: ReportType) -> str:
     url = get_url_without_page(URL, ImmoPlatform.KLEINANZEIGEN, type)
     if type == ReportType.LAND:
-        return url.replace('^^^', '7')
+        return url.replace("^^^", "7")
     else:
-        return url.replace('^^^', '8')
+        return url.replace("^^^", "8")
 
 
 def _get_results_of_type(type: ReportType):
@@ -46,11 +47,13 @@ def _get_results_of_type(type: ReportType):
     while True:
         url = _get_url_with_page(url_without_page, index)
         print(url)
-        soup = _get_soup(url, 'site-base--content')
-        new_listings = soup.find_all('li', {'class': 'ad-listitem'})
+        soup = _get_soup(url, "site-base--content")
+        new_listings = soup.find_all(
+            "li", {"class": "ad-listitem fully-clickable-card"}
+        )
         listings += list(map(lambda x: (x, _get_listing_soup(x)), new_listings))
         print(len(new_listings))
-        if len(new_listings) < 30:
+        if soup.find("a", {"class": "pagination-next"}) is None:
             break
         index += 1
 
@@ -62,7 +65,7 @@ def _get_listing_soup(listing):
     href = _get_href(listing)
     if href is None:
         return None
-    return _get_soup(f'{BASE_URL}{href}', 'site-base--content')
+    return _get_soup(f"{BASE_URL}{href}", "site-base--content")
 
 
 def _get_soup(url, class_name):
@@ -82,31 +85,45 @@ def _get_soup(url, class_name):
     html = driver.page_source
 
     # Parse the HTML response with BeautifulSoup
-    soup = BeautifulSoup(html, 'html.parser')
+    soup = BeautifulSoup(html, "html.parser")
 
     return soup
 
 
 def _get_immo_data(type: ReportType, listing) -> ImmoData:
     try:
-        title = listing[0].find('h2', {'class': 'text-module-begin'}).text.strip()
-        pattern = r'\(([^)]+)\)'
-        location_text = listing[0].find('div', {'class': 'aditem-main--top--left'}).text.strip()
+        title = listing[0].find("h2", {"class": "text-module-begin"}).text.strip()
+        pattern = r"\(([^)]+)\)"
+        location_text = (
+            listing[0].find("div", {"class": "aditem-main--top--left"}).text.strip()
+        )
         distance = re.findall(pattern, location_text)[0]
-        price = listing[1].find('h2', {'class': 'boxedarticle--price'}).text.strip()
+        price = listing[1].find("h2", {"class": "boxedarticle--price"}).text.strip()
         living_area = None
-        land_area = listing[1].find_all(lambda tag: tag.name == "li" and 'Grundstücksfläche' in tag.text)[0].find('span', {'class': 'addetailslist--detail--value'}).text.strip()
+        land_area = (
+            listing[1]
+            .find_all(lambda tag: tag.name == "li" and "Grundstücksfläche" in tag.text)[
+                0
+            ]
+            .find("span", {"class": "addetailslist--detail--value"})
+            .text.strip()
+        )
         if type == ReportType.HOUSE:
-            living_area = listing[1].find_all(lambda tag: tag.name == "li" and 'Wohnfläche' in tag.text)[0].find('span', {'class': 'addetailslist--detail--value'}).text.strip()
+            living_area = (
+                listing[1]
+                .find_all(lambda tag: tag.name == "li" and "Wohnfläche" in tag.text)[0]
+                .find("span", {"class": "addetailslist--detail--value"})
+                .text.strip()
+            )
         href = _get_href(listing[0])
         return ImmoData(
-            link=f'{BASE_URL}{href}',
+            link=f"{BASE_URL}{href}",
             title=title,
             price=price,
             living_area=living_area,
             land_area=land_area,
             type=type,
-            distance=distance
+            distance=distance,
         )
     except:
         return None
@@ -114,17 +131,17 @@ def _get_immo_data(type: ReportType, listing) -> ImmoData:
 
 def _get_url_with_page(url_without_page, index) -> str:
     if index == 1:
-        return url_without_page.replace('$$$', '')
+        return url_without_page.replace("$$$", "")
     else:
-        updated_url = url_without_page.replace('$$$', '/seite:$$$')
+        updated_url = url_without_page.replace("$$$", "/seite:$$$")
         return get_url_with_page(updated_url, index)
-   
-    
+
+
 def _get_href(listing) -> Optional[str]:
-    article = listing.find('article')
+    article = listing.find("article")
     if article is None:
         return None
     try:
-        return article['data-href']
+        return article["data-href"]
     except:
         return None
